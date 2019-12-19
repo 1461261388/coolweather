a678a83e6fd2277af9f490b9056037221d804a2d
1461261388 <1461261388@qq.com>
2019-12-19 11:02:39 +0800

    增加后台自动更新功能

--- app/src/main/java/com/example/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/coolweather/WeatherActivity.java
@@ -5,6 +5,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -222,6 +223,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this,AutoUpdateService.class);
+        startService(intent);
     }
 
 }
